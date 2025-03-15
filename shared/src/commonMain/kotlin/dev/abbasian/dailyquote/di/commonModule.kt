b824cad333ed322241c2ff13ba7c4b2b604e7948
Mahdi Abbasian <mam-1371@outlook.com>
2025-03-15 12:30:48 +0330

    init commonMain koin module

---
+++ shared/src/commonMain/kotlin/dev/abbasian/dailyquote/di/commonModule.kt
@@ -0,0 +1,34 @@
+package dev.abbasian.dailyquote.di
+
+import dev.abbasian.dailyquote.data.repository.QuoteRepository
+import dev.abbasian.dailyquote.data.repository.QuoteRepositoryImpl
+import dev.abbasian.dailyquote.domain.GetDailyQuoteUseCase
+import dev.abbasian.dailyquote.domain.GetFavoritesUseCase
+import dev.abbasian.dailyquote.domain.GetRandomQuoteUseCase
+import dev.abbasian.dailyquote.domain.RefreshQuotesUseCase
+import dev.abbasian.dailyquote.domain.ToggleFavoriteUseCase
+import dev.abbasian.dailyquote.presentation.QuoteViewModel
+import org.koin.core.context.startKoin
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+fun initKoin(platformModule: Module) {
+    startKoin {
+        modules(
+            commonModule(),
+            platformModule
+        )
+    }
+}
+
+fun commonModule() = module {
+    factory { GetDailyQuoteUseCase(get()) }
+    factory { GetFavoritesUseCase(get()) }
+    factory { ToggleFavoriteUseCase(get()) }
+    factory { GetRandomQuoteUseCase(get()) }
+    factory { RefreshQuotesUseCase(get()) }
+
+    single<QuoteRepository> { QuoteRepositoryImpl(get(), get()) }
+
+    factory { QuoteViewModel(get(), get(), get(), get(), get()) }
+}

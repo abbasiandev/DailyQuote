239fffb80960250cc3dcc18ecfa49e3ceba33923
Mahdi Abbasian <mam-1371@outlook.com>
2025-03-21 18:55:45 +0330

    modified iosModule for work correctly on xcode

--- shared/src/iosMain/kotlin/dev/abbasian/dailyquote/di/IosModule.kt
+++ shared/src/iosMain/kotlin/dev/abbasian/dailyquote/di/IosModule.kt
@@ -1,23 +1,79 @@
 package dev.abbasian.dailyquote.di
 
 import dev.abbasian.dailyquote.data.remote.QuoteRemoteDataSource
+import dev.abbasian.dailyquote.data.repository.QuoteRepositoryImpl
 import dev.abbasian.dailyquote.data.source.QuoteLocalDataSource
 import dev.abbasian.dailyquote.data.source.local.IOSQuoteLocalDataSource
 import dev.abbasian.dailyquote.data.source.remote.IOSQuoteRemoteDataSource
+import dev.abbasian.dailyquote.domain.GetDailyQuoteUseCase
+import dev.abbasian.dailyquote.domain.GetFavoritesUseCase
+import dev.abbasian.dailyquote.domain.GetRandomQuoteUseCase
+import dev.abbasian.dailyquote.domain.RefreshQuotesUseCase
+import dev.abbasian.dailyquote.domain.ToggleFavoriteUseCase
 import dev.abbasian.dailyquote.presentation.QuoteViewModel
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import org.koin.dsl.module
+import kotlin.experimental.ExperimentalObjCName
 
+@OptIn(ExperimentalObjCName::class)
+@kotlin.native.ObjCName(name = "doInitKoinIOS")
 fun initKoinIOS() = initKoin(
     module {
         single<QuoteLocalDataSource> { IOSQuoteLocalDataSource() }
         single<QuoteRemoteDataSource> { IOSQuoteRemoteDataSource() }
+
+        single { GetDailyQuoteUseCase(get()) }
+        single { GetFavoritesUseCase(get()) }
+        single { ToggleFavoriteUseCase(get()) }
+        single { GetRandomQuoteUseCase(get()) }
+        single { RefreshQuotesUseCase(get()) }
+
+        single {
+            QuoteViewModel(
+                getDailyQuoteUseCase = get(),
+                getFavoritesUseCase = get(),
+                toggleFavoriteUseCase = get(),
+                getRandomQuoteUseCase = get(),
+                refreshQuotesUseCase = get(),
+                coroutineScope = CoroutineScope(Dispatchers.Main)
+            )
+        }
     }
 )
 
-class QuoteHelper : KoinComponent {
-    private val viewModel: QuoteViewModel by inject()
+object KoinHelper {
+    @OptIn(ExperimentalObjCName::class)
+    @kotlin.native.ObjCName(name = "setup")
+    fun setup() = initKoinIOS()
+}
 
+@OptIn(ExperimentalObjCName::class)
+class QuoteHelper {
+    private val localDataSource = IOSQuoteLocalDataSource()
+    private val remoteDataSource = IOSQuoteRemoteDataSource()
+
+    private val quoteRepository by lazy {
+        QuoteRepositoryImpl(localDataSource, remoteDataSource)
+    }
+
+    private val getDailyQuoteUseCase by lazy { GetDailyQuoteUseCase(quoteRepository) }
+    private val getFavoritesUseCase by lazy { GetFavoritesUseCase(quoteRepository) }
+    private val toggleFavoriteUseCase by lazy { ToggleFavoriteUseCase(quoteRepository) }
+    private val getRandomQuoteUseCase by lazy { GetRandomQuoteUseCase(quoteRepository) }
+    private val refreshQuotesUseCase by lazy { RefreshQuotesUseCase(quoteRepository) }
+
+    private val viewModel by lazy {
+        QuoteViewModel(
+            getDailyQuoteUseCase,
+            getFavoritesUseCase,
+            toggleFavoriteUseCase,
+            getRandomQuoteUseCase,
+            refreshQuotesUseCase,
+            CoroutineScope(Dispatchers.Main)
+        )
+    }
+
+    @kotlin.native.ObjCName(name = "getViewModel")
     fun getViewModel(): QuoteViewModel = viewModel
 }

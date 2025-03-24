9d58c0f486ea100fd127166e86c06f230a6d03a8
Mahdi Abbasian <mam-1371@outlook.com>
2025-03-24 22:55:59 +0330

    set dark theme on swift

--- iosApp/iosApp/AppDelegate.swift
+++ iosApp/iosApp/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  iosApp
+//  DailyQuote
 //
 //  Created by Mahdi Abbasian on 3/21/25.
 //  Copyright © 2025 orgName. All rights reserved.
@@ -12,8 +12,17 @@ import shared
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Initialize Koin
+        //koin init
         shared.KoinHelper.initialize()
+        UIApplication.shared.windows.forEach { window in
+            window.overrideUserInterfaceStyle = .dark
+        }
         return true
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        UIApplication.shared.windows.forEach { window in
+            window.overrideUserInterfaceStyle = .dark
+        }
     }
 }
--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,18 +1,26 @@
 //
 //  iOSApp.swift
-//  iosApp
+//  DailyQuote
 //
 //  Created by Mahdi Abbasian on 3/21/25.
 //  Copyright © 2025 orgName. All rights reserved.
 //
 
 import SwiftUI
+import shared
 
 @main
 struct iOSApp: App {
-	var body: some Scene {
-		WindowGroup {
-			ContentView()
-		}
-	}
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    init() {
+        UIApplication.shared.windows.first?.overrideUserInterfaceStyle = .dark
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
 }
